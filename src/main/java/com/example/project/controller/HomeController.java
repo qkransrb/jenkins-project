85bc162f7c5d676eb47685f6b76a5b4fc0fa559c
qkransrb <qkransrb90@gmail.com>
2022-11-16 22:51:07 +0900

    initial commit

---
+++ src/main/java/com/example/project/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Jenkins Project v0.1";
+    }
+}
